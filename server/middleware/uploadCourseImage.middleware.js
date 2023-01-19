d76a26bb0d0abb0a33f8af86d20aac23f00d073c
dev.langesh <dev.langesh@gmail.com>
2023-01-19 13:03:11 +0530

    bug fix

--- server/middleware/uploadCourseImage.middleware.js
+++ server/middleware/uploadCourseImage.middleware.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.resolve(__dirname, "../public/images/course"));
+    cb(null, path.join(__dirname, "..", "public", "images", "course"));
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
--- server/middleware/uploadWork.middleware.js
+++ server/middleware/uploadWork.middleware.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.resolve(__dirname, "../public/work"));
+    cb(null, path.resolve(__dirname, "..", "public", "work"));
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
