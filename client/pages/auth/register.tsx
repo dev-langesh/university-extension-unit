4dc094aa9f67f8c673aba1b9a12f9d8c2f544a24
Langesh A <langesh@tiatech.net>
2023-02-08 14:44:37 +0530

    bug fix

--- client/pages/auth/register.tsx
+++ client/pages/auth/register.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import RegisterForm from "../../components/Auth/Register/RegisterForm";
 
 export default function Register() {
-  return <RegisterForm />;
+  return <RegisterForm type="student" />;
 }
